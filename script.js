5762b23875d4c6d56c26b6dcde374d59fb252a57
Vincy-2515 <110780936+Vincy-2515@users.noreply.github.com>
2026-06-03 22:27:01 +0200

    Code reformatting

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function showPage(p) {
-  document.querySelectorAll('.page').forEach(x => x.classList.remove('active'));
-  document.getElementById('page-' + p).classList.add('active');
-  document.getElementById('nav-home').classList.toggle('active', p === 'home');
-  document.getElementById('nav-content').classList.toggle('active', p === 'content1' || p === 'content2');
-  window.scrollTo(0, 0);
+    document.querySelectorAll('.page').forEach(x => x.classList.remove('active'));
+    document.getElementById('page-' + p).classList.add('active');
+    document.getElementById('nav-home').classList.toggle('active', p === 'home');
+    document.getElementById('nav-content').classList.toggle('active', p === 'content1' || p === 'content2');
+    window.scrollTo(0, 0);
 }
